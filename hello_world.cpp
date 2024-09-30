0a755b7b298625c305c77d0877dfb7af6c0bd543
sekhmych <sekhmych@gmail.com>
2024-09-30 15:54:46 +0900

    Update hello_world.cpp comments

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,15 +1,19 @@
-#include <iostream>
 #include <string>
+#include <iostream>
 
-// Entry point
+// Entry точка
 int main() {
-	// name hold user name
+	// name hold имя пользователя
 	std::string name;
 	// programm need user name
 	std::cout << "Please, enter your name: ";
 	std::cin >> name;
+	// Процесс вывода в поток вывода, который в консоль
 	// output - user greetings world
 	std::cout << "Hello world from " << name << std::endl;
+
+	// Стоит поменять текст, как нибудь потом
+	
 	return 0;
 }
 
