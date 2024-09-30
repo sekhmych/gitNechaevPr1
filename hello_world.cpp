c7028681890290372038de8bec65f07f24205eaa
sekhmych <sekhmych@gmail.com>
2024-09-30 06:04:29 +0000

    Add comments to hello_world.cpp

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,10 +1,14 @@
 #include <iostream>
 #include <string>
 
+// Entry point
 int main() {
+	// name hold user name
 	std::string name;
+	// programm need user name
 	std::cout << "Please, enter your name: ";
 	std::cin >> name;
+	// output - user greetings world
 	std::cout << "Hello world from " << name << std::endl;
 	return 0;
 }
